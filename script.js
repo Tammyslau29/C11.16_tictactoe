6c114b494eaebec610cb2cafe2487b5b11783455
Tammyslau29 <Tammyslau29@gmail.com>
2016-11-30 12:33:08 -0800

    Click Handlers on Div

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
  * Created by sloan on 11/29/2016.
  */
 
+/*Generates array of winning numbers*/
 var generateWinningNumbers = function(size){
     var val = 1, cells = [], wins = [];
     for (var i = 0; i < size; i++) {
@@ -9,8 +10,8 @@ var generateWinningNumbers = function(size){
         for (var j = 0; j < size; j++){
             cells[i][j] = val;
             val *=2;
-        }
-    }
+        };
+    };
     var row_wins = [], col_wins = [], first_diag_win = 0, second_diag_win=0;
     for (i = 0; i < size; i++){
         row_wins[i]=0;
@@ -20,19 +21,25 @@ var generateWinningNumbers = function(size){
         for (j=0; j< size; j++){
             row_wins[i] += cells[i][j]
             col_wins[i] += cells[j][i]
-        }
-    }
+        };
+    };
     return row_wins.concat(col_wins, first_diag_win, second_diag_win);
 };
-var winning_array = generateWinningNumbers(game_size)
-
-console.log(winning_array)
-//Checks player score against winning numbers and returns result//
+var winning_array = generateWinningNumbers(game_size);
+/*Checks player score against winning numbers and returns result*/
 var winningScore = function(score){
     for (var i = 0; i < winning_array.length; i++){
         if (winning_array[i] & player_score === winning_array[i]){
             return true
-        }
+        };
         return false
-    }
-}
+    };
+};
+$(document).ready(function() {
+    var turn = "X";
+    $(this).click(function(){
+        console.log("Works")
+        $(this).html(turn)
+        turn = turn === "X" ? "O" : "X";
+    })
+})
